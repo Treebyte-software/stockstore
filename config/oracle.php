c7bb2e52e0dccd27642b23d6452c393133b12ec4
System Administrator <root@MacBook-Pro-di-Alessandro.local>
2019-11-18 17:13:27 +0100

    configuration connection laravel oracle

--- config/oracle.php
+++ config/oracle.php
@@ -2,17 +2,14 @@
 
 return [
     'oracle' => [
-        'driver'         => 'oracle',
-        'tns'            => env('DB_TNS', ''),
-        'host'           => env('DB_HOST', ''),
-        'port'           => env('DB_PORT', '1521'),
-        'database'       => env('DB_DATABASE', ''),
-        'username'       => env('DB_USERNAME', ''),
-        'password'       => env('DB_PASSWORD', ''),
-        'charset'        => env('DB_CHARSET', 'AL32UTF8'),
-        'prefix'         => env('DB_PREFIX', ''),
-        'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
-        'edition'        => env('DB_EDITION', 'ora$base'),
-        'server_version' => env('DB_SERVER_VERSION', '11g'),
-    ],
+        'driver' => 'oracle',
+        'host' => '10.18.101.6',
+        'port' => '1521',
+        'database' => 'orcl',
+        'service_name' => 'sid_alias',
+        'username' => 'ss_rhg',
+        'password' => 'treebyte',
+        'charset' => 'AL32UTF8',
+        'prefix' => '',
+    ]
 ];
