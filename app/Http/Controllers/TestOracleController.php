52bb8d1bdb158ea1b1e49b7cab6de6f322266d6b
alessandro.zampieri <alessandro.zampieri@treebyte.com>
2019-11-19 11:30:39 +0100

    add route and select companies in Oracle Companies

--- app/Http/Controllers/TestOracleController.php
+++ app/Http/Controllers/TestOracleController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestOracleController extends Controller
 {
-    //
+    // test select companies oracle
+
+    public function test()
+    {
+        $companies = DB::select('select * from sst_companies');
+
+        var_dump($companies);
+    }
 }
